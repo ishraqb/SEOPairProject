354fb7595e17fd8eede58a823316a1b621b6fecf
77yahir <77yahir80@gmail.com>
2026-06-23 15:38:46 -0700

    Outlined the planner.py for making decisions and creating the 3 different plans

---
+++ planner.py
@@ -0,0 +1,17 @@
+## Decides what goes into each plan
+
+def build_cheapest_plan():
+    pass
+
+def build_balanced_plan():
+    pass
+
+def build_experience_plan():
+    pass
+
+def calculate_total_cost():
+    pass
+
+def is_within_budget():
+    pass
+
